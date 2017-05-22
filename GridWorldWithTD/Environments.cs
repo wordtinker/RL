892f605f90c592e44539d399fc0495b203d3eab9
wordtinker <wordtinker@yandex.ru>
2017-05-22 22:47:02 +0300

    SARSA

---
+++ GridWorldWithTD/Environments.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GridWorldWithTD
+{
+
+    public enum Action
+    {
+        Up, Down, Left, Right
+    }
+
+    public enum StateType
+    {
+        Terminal, NonTerminal
+    }
+
+    public class State
+    {
+        internal int i;
+        internal int j;
+        public StateType Type { get; internal set; }
+
+        public IEnumerable<Action> Actions
+        {
+            get
+            {
+                if (Type == StateType.NonTerminal)
+                {
+                    foreach (var item in Enum.GetValues(typeof(Action)).Cast<Action>())
+                    {
+                        yield return item;
+                    }
+                }
+                else
+                {
+                    yield break;
+                }
+            }
+        }
+        public State(int i, int j, StateType type = StateType.NonTerminal)
+        {
+            this.i = i;
+            this.j = j;
+            this.Type = type;
+        }
+    }
+
+    abstract class AEnvironment
+    {
+        public abstract IEnumerable<State> StatesPlus { get; }
+        public abstract IEnumerable<State> States { get; }
+        public abstract Tuple<State, double> GetReward(State state, Action action);
+        public abstract void Print();
+        public abstract void PrintVs(Policy policy);
+        public abstract void PrintsSAV(Policy policy);
+        public abstract void PrintPolicy(Policy policy);
+    }
+
+    abstract class Grid : AEnvironment
+    {
+        protected State[][] states;
+
+        public Grid(int x, int y)
+        {
+            states = new State[x][];
+            for (int i = 0; i < x; i++)
+            {
+                states[i] = new State[y];
+                for (int j = 0; j < y; j++)
+                {
+                    states[i][j] = new State(i, j);
+                }
+            }
+        }
+
+        public override IEnumerable<State> StatesPlus
+        {
+            get
+            {
+                foreach (var item in states)
+                {
+                    foreach (State s in item)
+                    {
+                        yield return s;
+                    }
+                }
+            }
+        }
+        public override IEnumerable<State> States
+        {
+            get
+            {
+                foreach (var item in states)
+                {
+                    foreach (State s in item)
+                    {
+                        if (s.Type != StateType.Terminal) yield return s;
+                    }
+                }
+            }
+        }
+        public override void Print()
+        {
+            foreach (var item in states)
+            {
+                foreach (State s in item)
+                {
+                    if (s.Type == StateType.NonTerminal)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write("* ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        public override void PrintVs(Policy policy)
+        {
+            foreach (var item in states)
+            {
+                foreach (State s in item)
+                {
+                    Console.Write($"{policy.V(s):N2} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public override void PrintsSAV(Policy policy)
+        {
+            foreach (var s in States)
+            {
+                foreach (var kvp in policy.A(s))
+                {
+                    Console.WriteLine($"{s.i} {s.j} {kvp.Key} {kvp.Value.Value:N2} {kvp.Value.N}");
+                }
+                Console.WriteLine();
+            }
+        }
+        public override void PrintPolicy(Policy policy)
+        {
+            foreach (var item in states)
+            {
+                string line1 = "";
+                string line2 = "";
+                string line3 = "";
+                string line4 = "";
+                string line5 = "";
+                foreach (State s in item)
+                {
+                    if (s.Type == StateType.Terminal)
+                    {
+                        line1 += "xxxxxxxxxxx ";
+                        line2 += "xxxxxxxxxxx ";
+                        line3 += "xxxxxxxxxxx ";
+                        line4 += "xxxxxxxxxxx ";
+                        line5 += "xxxxxxxxxxx ";
+                    }
+                    else
+                    {
+                        string empty = "            ";
+                        var actions = policy.A(s);
+                        var partial = actions.Where(kvp => kvp.Value.InPolicy).Select(kvp => kvp.Key);
+                        line1 += partial.Contains(Action.Up) ? "     ^      " : empty;
+                        line2 += partial.Contains(Action.Up) ? $"    {actions[Action.Up].P:N2}    " : empty;
+                        line3 += partial.Contains(Action.Left) ? $"<{actions[Action.Left].P:N2} " : "      ";
+                        line3 += partial.Contains(Action.Right) ? $" {actions[Action.Right].P:N2}>" : "      ";
+                        line4 += partial.Contains(Action.Down) ? $"    {actions[Action.Down].P:N2}    " : empty;
+                        line5 += partial.Contains(Action.Down) ? "     v      " : empty;
+                    }
+                }
+                Console.WriteLine(line1);
+                Console.WriteLine(line2);
+                Console.WriteLine(line3);
+                Console.WriteLine(line4);
+                Console.WriteLine(line5);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    /// <summary>
+    /// A standard gridworld,
+    /// with start and goal states, but with one difference: there is a crosswind upward
+    /// through the middle of the grid.The actions are the standard four—up, down,
+    /// right, and left—but in the middle region the resultant next states are shifted upward
+    /// by a “wind,” the strength of which varies from column to column.
+    /// </summary>
+    class WindyGrid : Grid
+    {
+        /// <summary>
+        /// Hidden information.
+        /// </summary>
+        /// <param name="state"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        private State NextState(State state, Action action)
+        {
+            int nexti;
+            int nextj;
+            switch (action)
+            {
+                // probability of every action is 100%
+                case Action.Up:
+                    nexti = state.i - 1;
+                    nextj = state.j;
+                    break;
+                case Action.Down:
+                    nexti = state.i + 1;
+                    nextj = state.j;
+                    break;
+                case Action.Left:
+                    nexti = state.i;
+                    nextj = state.j - 1;
+                    break;
+                case Action.Right:
+                    nexti = state.i;
+                    nextj = state.j + 1;
+                    break;
+                default:
+                    nexti = state.i;
+                    nextj = state.j;
+                    break;
+            }
+
+            // apply wind
+            if (nextj > 2 && nextj < 9) nexti--;
+            if (nextj > 5 && nextj < 8) nexti--;
+
+            // fix out of bounds error
+            if (nexti < 0) nexti = 0;
+            if (nexti > 6) nexti = 6;
+            if (nextj < 0) nextj = 0;
+            if (nextj > 9) nextj = 9;
+
+            return states[nexti][nextj];
+        }
+        /// <summary>
+        /// Do not have to know next state, not like in DP solution.
+        /// </summary>
+        /// <param name="state"></param>
+        /// <param name="action"></param>
+        /// <returns>New stat and reward </returns>
+        public override Tuple<State, double> GetReward(State state, Action action)
+        {
+            State newState = NextState(state, action);
+            if (newState.Type == StateType.Terminal)
+            {
+                return Tuple.Create(newState, 3.00);
+            }
+            return Tuple.Create(newState, -1.00);
+        }
+        public WindyGrid() : base(7, 10)
+        {
+            states[3][7].Type = StateType.Terminal;
+        }
+    }
+
+    /// <summary>
+    /// Simple grid with two exits.
+    /// </summary>
+    class GridWorld : Grid
+    {
+        /// <summary>
+        /// Hidden information.
+        /// </summary>
+        /// <param name="state"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        private State NextState(State state, Action action)
+        {
+            try
+            {
+                switch (action)
+                {
+                    // probability of every action is 100%
+                    case Action.Up:
+                        return states[state.i - 1][state.j];
+                    case Action.Down:
+                        return states[state.i + 1][state.j];
+                    case Action.Left:
+                        return states[state.i][state.j - 1];
+                    case Action.Right:
+                        return states[state.i][state.j + 1];
+                    default:
+                        return state;
+                }
+            }
+            catch (Exception)
+            {
+                return state;
+            }
+        }
+        /// <summary>
+        /// Do not have to know next state, not like in DP solution.
+        /// </summary>
+        /// <param name="state"></param>
+        /// <param name="action"></param>
+        /// <returns>New stat and reward </returns>
+        public override Tuple<State, double> GetReward(State state, Action action)
+        {
+            State newState = NextState(state, action);
+            if (newState.Type == StateType.Terminal)
+            {
+                return Tuple.Create(newState, 1.00);
+            }
+            else if (state == newState)
+            {
+                return Tuple.Create(newState, -2.00);
+            }
+            return Tuple.Create(newState, -1.00);
+        }
+
+        public GridWorld() : base(4, 4)
+        {
+            states[0][0].Type = StateType.Terminal;
+            states[3][3].Type = StateType.Terminal;
+        }
+    }
+}
---
+++ GridWorldWithTD/Program.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GridWorldWithTD
+{
+    class PolicyState
+    {
+        private Random rnd;
+        // state value
+        public double? Value { get; set; }
+        // state return
+        public double? G { get; set; }
+        // number of times state has been visited(first time)
+        public int N { get; set; }
+        // list of actions
+        public Dictionary<Action, StateActionPolicy> Actions { get; }
+        public void Rebalance(double epsilon = 0.2)
+        {
+            double? max = Actions.Max(kvp => kvp.Value.Value);
+            double size = Actions.Where(kvp => kvp.Value.Value == max).Count();
+            if (epsilon == 0)
+            {
+                // policy is greedy
+                foreach (var item in Actions)
+                {
+                    item.Value.P = item.Value.Value == max ? 1 / size : 0;
+                }
+            }
+            else
+            {
+                // soft policy
+                double x = (1 - epsilon) / size + epsilon / (double)Actions.Count;
+                foreach (var item in Actions)
+                {
+                    item.Value.P = item.Value.Value == max ? x : epsilon / (double)Actions.Count;
+                }
+            }
+        }
+        public Action NextAction
+        {
+            get
+            {
+                // choose action randomly
+                // any policy is supported(greedy, soft-epsilon)
+                double ra = rnd.NextDouble();
+                double accumulator = 0;
+                foreach (var kvp in Actions.Where(sap => sap.Value.InPolicy))
+                {
+                    accumulator += kvp.Value.P;
+                    if (ra < accumulator)
+                    {
+                        return kvp.Key;
+                    }
+                }
+                // normally it would have returned already, but if
+                // due to uneven division of probabilities
+                // ra was ever less than accumulated probability
+                // return last key
+                return Actions.Keys.Last();
+            }
+        }
+        public PolicyState(State s)
+        {
+            rnd = new Random();
+            Actions = new Dictionary<Action, StateActionPolicy>();
+            double size = s.Actions.Count();
+            foreach (Action a in s.Actions)
+            {
+                Actions[a] = new StateActionPolicy();
+                Actions[a].P = 1 / size;
+            }
+        }
+    }
+
+    class StateActionPolicy
+    {
+        // state action value
+        public double? Value { get; set; }
+        // cumulative sum Cn of the weights given to the first n returns for off policy learning
+        public double C { get; set; }
+        // state action return
+        public double? G { get; set; }
+        // number of times state action has been visited(first time or every time)
+        public int N { get; set; }
+        // marks action as used by Policy
+        public bool InPolicy { get { return P > 0; } }
+        // probability of activation in policy
+        public double P { get; set; }
+
+        public StateActionPolicy()
+        {
+            Value = null;
+        }
+    }
+
+    static class Walker
+    {
+        private static Random rnd = new Random();
+        private static IEnumerable<Tuple<State, Action, double>> GenerateWalk(AEnvironment env, Policy p, State s, Action a)
+        {
+            // Reward
+            double r;
+            while (true)
+            {
+                // ask for new state and reward
+                var nsr = env.GetReward(s, a);
+                r = nsr.Item2;
+
+                yield return Tuple.Create(s, a, r);
+
+                s = nsr.Item1;
+                if (s.Type == StateType.Terminal)
+                {
+                    break;
+                }
+                // choose action by the policy
+                a = p.P[s].NextAction;
+            }
+        }
+
+        /// <summary>
+        /// Generates an episode starting from S0, A0, following π
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Tuple<State, Action, double>> GenerateEpisode(AEnvironment env, Policy p, bool exploringStart = true)
+        {
+            int startingState = rnd.Next(env.States.Count());
+            State s = env.States.ElementAt(startingState);
+            Action a;
+            if (exploringStart)
+            {
+                // Exploring start
+                // any s,a pair is viable as starting pair
+                // regardless of policy
+                int startingAction = rnd.Next(s.Actions.Count());
+                a = s.Actions.ElementAt(startingAction);
+            }
+            else
+            {
+                // choose action by the policy
+                a = p.P[s].NextAction;
+            }
+            // Generate a walk from that position
+            foreach (var item in GenerateWalk(env, p, s, a))
+            {
+                yield return item;
+            }
+        }
+    }
+
+    class Policy
+    {
+        public Dictionary<State, PolicyState> P { get; }
+        private AEnvironment env;
+        private double gamma;
+        private Random rnd;
+
+        public double V(State s)
+        {
+            return P[s].Value.GetValueOrDefault(double.MinValue);
+        }
+
+        public double V(State s, Action a)
+        {
+            return P[s].Actions[a].Value.GetValueOrDefault(double.MinValue);
+        }
+
+        public Dictionary<Action, StateActionPolicy> A(State s)
+        {
+            return P[s].Actions;
+        }
+
+        public int SARSA(int limit = 1000, double alpha = 0.5)
+        {
+            for (int i = 0; i < limit; i++)
+            {
+                // Generate an episode
+                // episode MUST be IEnumerable so policy can be evaluated
+                // at runtime
+                var episode = Walker.GenerateEpisode(env, this, exploringStart: false);
+                var enumer = episode.GetEnumerator();
+                // move to first element
+                enumer.MoveNext();
+                // chose A from S using policy
+                State s = enumer.Current.Item1;
+                Action a = enumer.Current.Item2;
+                double r = enumer.Current.Item3;
+                // for every action next in the episode
+                foreach (var sar in episode)
+                {
+                    // observe next state and next action using policy
+                    State nextState = sar.Item1;
+                    Action nextAction = sar.Item2;
+                    // Update Q for s-a
+                    StateActionPolicy sap = P[s].Actions[a];
+                    double currentValue = sap.Value.GetValueOrDefault(0);
+                    double nextSAValue = P[nextState].Actions[nextAction].Value.GetValueOrDefault(0);
+                    sap.Value = currentValue + alpha * (r + gamma * nextSAValue - currentValue);
+                    sap.N++;
+                    // rebalance probabilities of current policy
+                    P[s].Rebalance(epsilon: 0.2);
+                    r = sar.Item3;
+                    s = nextState;
+                    a = nextAction;
+                }
+            }
+            return limit;
+        }
+
+        public Policy(AEnvironment env, double gamma = 1)
+        {
+            this.rnd = new Random();
+            this.env = env;
+            this.gamma = gamma;
+            // create equiprobable random policy
+            P = new Dictionary<State, PolicyState>();
+            foreach (State s in env.StatesPlus)
+            {
+                P[s] = new PolicyState(s);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void UseSARSA(AEnvironment env)
+        {
+            Console.WriteLine("Initial gridworld");
+            env.Print();
+            Console.WriteLine();
+
+            Policy p = new Policy(env, gamma: 0.5);
+            Console.WriteLine("Starting policy");
+            env.PrintPolicy(p);
+            Console.WriteLine();
+
+            p.SARSA(limit: 100000, alpha: 0.5);
+            env.PrintPolicy(p);
+            env.PrintsSAV(p);
+            // show as greedy policy
+            foreach (var item in p.P.Values)
+            {
+                item.Rebalance(0);
+            }
+            env.PrintPolicy(p);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 - SARSA(On-policy) on Grid World");
+            Console.WriteLine("2 - SARSA(On-policy) on Windy World");
+            string decision = Console.ReadLine();
+            AEnvironment env;
+            switch (decision)
+            {
+                case "1":
+                    env = new GridWorld();
+                    UseSARSA(env);
+                    break;
+                case "2":
+                    env = new WindyGrid();
+                    UseSARSA(env);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}

6bf8797b301a7213e8db2e0ced62be2dc7dcf63e
wordtinker <wordtinker@yandex.ru>
2017-05-26 20:04:00 +0300

    MCTS

---
+++ MonteCarloTreeSearch/Environment.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MonteCarloTreeSearch
+{
+
+    public enum Action
+    {
+        Up, Down, Left, Right
+    }
+
+    public enum StateType
+    {
+        Terminal, NonTerminal
+    }
+
+    public class State
+    {
+        internal int i;
+        internal int j;
+        public StateType Type { get; internal set; }
+
+        public IEnumerable<Action> Actions
+        {
+            get
+            {
+                if (Type == StateType.NonTerminal)
+                {
+                    foreach (var item in Enum.GetValues(typeof(Action)).Cast<Action>())
+                    {
+                        yield return item;
+                    }
+                }
+                else
+                {
+                    yield break;
+                }
+            }
+        }
+        public State(int i, int j, StateType type = StateType.NonTerminal)
+        {
+            this.i = i;
+            this.j = j;
+            this.Type = type;
+        }
+    }
+
+    abstract class AEnvironment
+    {
+        public abstract IEnumerable<State> StatesPlus { get; }
+        public abstract IEnumerable<State> States { get; }
+        public abstract State StartState { get; }
+        public abstract Tuple<State, double> GetReward(State state, Action action);
+        public abstract void Print();
+        public abstract void PrintPolicy(IPolicy policy);
+    }
+
+    abstract class Grid : AEnvironment
+    {
+        protected State[][] states;
+
+        public Grid(int x, int y)
+        {
+            states = new State[x][];
+            for (int i = 0; i < x; i++)
+            {
+                states[i] = new State[y];
+                for (int j = 0; j < y; j++)
+                {
+                    states[i][j] = new State(i, j);
+                }
+            }
+        }
+
+        public override IEnumerable<State> StatesPlus
+        {
+            get
+            {
+                foreach (var item in states)
+                {
+                    foreach (State s in item)
+                    {
+                        yield return s;
+                    }
+                }
+            }
+        }
+        public override IEnumerable<State> States
+        {
+            get
+            {
+                foreach (var item in states)
+                {
+                    foreach (State s in item)
+                    {
+                        if (s.Type != StateType.Terminal) yield return s;
+                    }
+                }
+            }
+        }
+        public override void Print()
+        {
+            foreach (var item in states)
+            {
+                foreach (State s in item)
+                {
+                    if (s.Type == StateType.NonTerminal)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write("* ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        public override void PrintPolicy(IPolicy policy)
+        {
+            string empty = "            ";
+            foreach (var item in states)
+            {
+                string line1 = "";
+                string line2 = "";
+                string line3 = "";
+                string line4 = "";
+                string line5 = "";
+                foreach (State s in item)
+                {
+                    if (s.Type == StateType.Terminal)
+                    {
+                        line1 += "xxxxxxxxxxx ";
+                        line2 += "xxxxxxxxxxx ";
+                        line3 += "xxxxxxxxxxx ";
+                        line4 += "xxxxxxxxxxx ";
+                        line5 += "xxxxxxxxxxx ";
+                    }
+                    else if (policy.ContainsState(s))
+                    {
+                        var actions = policy.A(s);
+                        var partial = actions.Where(kvp => kvp.Value.InPolicy).Select(kvp => kvp.Key);
+                        line1 += partial.Contains(Action.Up) ? "     ^      " : empty;
+                        line2 += partial.Contains(Action.Up) ? $"    {actions[Action.Up].P:N2}    " : empty;
+                        line3 += partial.Contains(Action.Left) ? $"<{actions[Action.Left].P:N2} " : "      ";
+                        line3 += partial.Contains(Action.Right) ? $" {actions[Action.Right].P:N2}>" : "      ";
+                        line4 += partial.Contains(Action.Down) ? $"    {actions[Action.Down].P:N2}    " : empty;
+                        line5 += partial.Contains(Action.Down) ? "     v      " : empty;
+                    }
+                    else
+                    {
+                        line1 += empty;
+                        line2 += empty;
+                        line3 += empty;
+                        line4 += empty;
+                        line5 += empty;
+                    }
+                }
+                Console.WriteLine(line1);
+                Console.WriteLine(line2);
+                Console.WriteLine(line3);
+                Console.WriteLine(line4);
+                Console.WriteLine(line5);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class ProxyWind : AEnvironment
+    {
+        private WindyGrid env;
+        private List<State> visited;
+        public ProxyWind(WindyGrid env)
+        {
+            this.env = env;
+            this.visited = new List<State>();
+        }
+
+        public override State StartState
+        {
+            get
+            {
+                return env.StartState;
+            }
+        }
+        public override IEnumerable<State> States
+        {
+            get
+            {
+                return env.States;
+            }
+        }
+        public override IEnumerable<State> StatesPlus
+        {
+            get
+            {
+                return env.StatesPlus;
+            }
+        }
+        //public override Tuple<State, double> GetReward(State state, Action action)
+        //{
+        //    n++;
+        //    var sr = env.GetReward(state, action);
+        //    double r = sr.Item2 - n;
+        //    if (sr.Item1.Type == StateType.Terminal) n = 0;
+        //    return Tuple.Create(sr.Item1, r);
+        //}
+        public override Tuple<State, double> GetReward(State state, Action action)
+        {
+            visited.Add(state);
+            var sr = env.GetReward(state, action);
+            State s = sr.Item1;
+            double r = sr.Item2;
+            if (visited.Contains(s))
+            {
+                visited.Clear();
+                return Tuple.Create(new State(-1, -1, StateType.Terminal), -1.00);
+            }
+            if (s.Type == StateType.Terminal) visited.Clear();
+            return sr;
+        }
+        public override void Print()
+        {
+            env.Print();
+        }
+        public override void PrintPolicy(IPolicy policy)
+        {
+            env.PrintPolicy(policy);
+        }
+    }
+
+    /// <summary>
+    /// A standard gridworld,
+    /// with start and goal states, but with one difference: there is a crosswind upward
+    /// through the middle of the grid.The actions are the standard four—up, down,
+    /// right, and left—but in the middle region the resultant next states are shifted upward
+    /// by a “wind,” the strength of which varies from column to column.
+    /// </summary>
+    class WindyGrid : Grid
+    {
+        public override State StartState
+        {
+            get
+            {
+                return states[3][0];
+            }
+        }
+
+        /// <summary>
+        /// Hidden information.
+        /// </summary>
+        /// <param name="state"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        private State NextState(State state, Action action)
+        {
+            int nexti;
+            int nextj;
+            switch (action)
+            {
+                // probability of every action is 100%
+                case Action.Up:
+                    nexti = state.i - 1;
+                    nextj = state.j;
+                    break;
+                case Action.Down:
+                    nexti = state.i + 1;
+                    nextj = state.j;
+                    break;
+                case Action.Left:
+                    nexti = state.i;
+                    nextj = state.j - 1;
+                    break;
+                case Action.Right:
+                    nexti = state.i;
+                    nextj = state.j + 1;
+                    break;
+                default:
+                    nexti = state.i;
+                    nextj = state.j;
+                    break;
+            }
+
+            // apply wind
+            //if (nextj > 4 && nextj < 9) nexti--;
+            //if (nextj > 5 && nextj < 8) nexti--;
+
+            // fix out of bounds error
+            if (nexti < 0) nexti = 0;
+            if (nexti > 6) nexti = 6;
+            if (nextj < 0) nextj = 0;
+            if (nextj > 9) nextj = 9;
+
+            return states[nexti][nextj];
+        }
+        /// <summary>
+        /// Do not have to know next state, not like in DP solution.
+        /// </summary>
+        /// <param name="state"></param>
+        /// <param name="action"></param>
+        /// <returns>New stat and reward </returns>
+        public override Tuple<State, double> GetReward(State state, Action action)
+        {
+            State newState = NextState(state, action);
+            if (newState.Type == StateType.Terminal)
+            {
+                return Tuple.Create(newState, 3.00);
+            }
+            return Tuple.Create(newState, -1.00);
+        }
+        public WindyGrid() : base(7, 10)
+        {
+            states[3][7].Type = StateType.Terminal;
+        }
+    }
+}
---
+++ MonteCarloTreeSearch/Program.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MonteCarloTreeSearch
+{
+    class PolicyState
+    {
+        // the exploration parameter
+        private double c = 0.01;
+        private Random rnd;
+        // number of times state has been visited
+        public int N { get; set; }
+        // list of actions
+        public Dictionary<Action, StateActionPolicy> Actions { get; }
+        public void Rebalance()
+        {
+            // using upper confidence bound as value
+            foreach (StateActionPolicy p in Actions.Values)
+            {
+                if (p.N == 0)
+                {
+                    p.Value = double.PositiveInfinity;
+                }
+                else
+                {
+                    p.Value = p.W / p.N + c * Math.Sqrt(Math.Log(N) / p.N);
+                }
+            }
+
+            // Update probabilities of calling action
+            double max = Actions.Values.Max(p => p.Value);
+            double size = Actions.Where(kvp => kvp.Value.Value == max).Count();
+            // policy is greedy
+            foreach (var item in Actions)
+            {
+                item.Value.P = item.Value.Value == max ? 1 / size : 0;
+            }
+            
+        }
+        public Action NextAction
+        {
+            get
+            {
+                // choose action randomly
+                // any policy is supported(greedy, soft-epsilon)
+                double ra = rnd.NextDouble();
+                double accumulator = 0;
+                foreach (var kvp in Actions.Where(sap => sap.Value.InPolicy))
+                {
+                    accumulator += kvp.Value.P;
+                    if (ra < accumulator)
+                    {
+                        return kvp.Key;
+                    }
+                }
+                // normally it would have returned already, but if
+                // due to uneven division of probabilities
+                // ra was ever less than accumulated probability
+                // return last key
+                return Actions.Keys.Last();
+            }
+        }
+        public PolicyState(State s)
+        {
+            rnd = new Random();
+            Actions = new Dictionary<Action, StateActionPolicy>();
+            double size = s.Actions.Count();
+            foreach (Action a in s.Actions)
+            {
+                Actions[a] = new StateActionPolicy();
+                Actions[a].P = 1 / size;
+            }
+        }
+    }
+    class StateActionPolicy
+    {
+        // state action value
+        public double Value { get; set; }
+        // number of times state action has been visited
+        public int N { get; set; }
+        // number of winning times
+        public int W { get; set; }
+        // marks action as used by Policy
+        public bool InPolicy { get { return P > 0; } }
+        // probability of activation in policy
+        public double P { get; set; }
+        public StateActionPolicy()
+        {
+            Value = 0;
+        }
+    }
+
+    interface IPolicy
+    {
+        void AddState(State s);
+        bool ContainsState(State s);
+        Action GetAction(State s);
+        void Update(State s, Action a, double r);
+        Dictionary<Action, StateActionPolicy> A(State s);
+    }
+
+    class SoftPolicy : IPolicy
+    {
+        public Dictionary<State, PolicyState> p = new Dictionary<State, PolicyState>();
+
+        public Dictionary<Action, StateActionPolicy> A(State s)
+        {
+            return p[s].Actions;
+        }
+
+        public void AddState(State s)
+        {
+            p[s] = new PolicyState(s);
+        }
+
+        public bool ContainsState(State s)
+        {
+            return p.ContainsKey(s);
+        }
+
+        public Action GetAction(State s)
+        {
+            return p[s].NextAction;
+        }
+
+        public void Update(State s, Action a, double r)
+        {
+            p[s].N++;
+            p[s].Actions[a].N++;
+            if (r > 0)
+            {
+                p[s].Actions[a].W++;
+
+            }
+            p[s].Rebalance();
+        }
+    }
+
+    class RandomPolicy : IPolicy
+    {
+        private PolicyState policyState;
+
+        public Dictionary<Action, StateActionPolicy> A(State s)
+        {
+            return policyState.Actions;
+        }
+
+        public void AddState(State s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsState(State s)
+        {
+            return true;
+        }
+
+        public Action GetAction(State s)
+        {
+            return policyState.NextAction;
+        }
+        public void Update(State s, Action a, double r)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RandomPolicy()
+        {
+            State dummy = new State(-1, -1, StateType.NonTerminal);
+            policyState = new PolicyState(dummy);
+        }
+    }
+
+    class MCTS
+    {
+        private AEnvironment env;
+        private State startState;
+        private IPolicy policy;
+        private IPolicy simulator;
+        private Stack<Tuple<State, Action>> episode;
+        private double? Selection()
+        {
+            State s = startState;
+            while (policy.ContainsState(s))
+            {
+                Action a = policy.GetAction(s);
+                var sr = env.GetReward(s, a);
+                episode.Push(Tuple.Create(s, a));
+                s = sr.Item1;
+                if (s.Type == StateType.Terminal) return sr.Item2;
+            }
+            // Phase 2. Expand tree
+            Expansion(s);
+            return null;
+        }
+        private void Expansion(State s)
+        {
+            policy.AddState(s);
+        }
+        private double Simulation()
+        {
+            State s = episode.Peek().Item1;
+            double r;
+            do
+            {
+                Action a = simulator.GetAction(s);
+                var sr = env.GetReward(s, a);
+                s = sr.Item1;
+                r = sr.Item2;
+            } while (s.Type != StateType.Terminal);
+            return r;
+        }
+        private void Update(double reward)
+        {
+            while (episode.Count > 0)
+            {
+                var sa = episode.Pop();
+                policy.Update(sa.Item1, sa.Item2, reward);
+            }
+        }
+        public void UpdatePolicy(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                double? finalReward = null;
+                // Phase 1. Select moves while we have state in policy
+                finalReward = Selection();
+                // Phase 3. Simulate the tail of episode
+                if (!finalReward.HasValue) finalReward = Simulation();
+                // Phase 4. Back-propagation
+                Update(finalReward.Value);    
+            }
+        }
+        public MCTS(AEnvironment env, State startState, IPolicy policy, IPolicy simulator)
+        {
+            this.env = env;
+            this.startState = startState;
+            this.policy = policy;
+            this.simulator = simulator;
+            episode = new Stack<Tuple<State, Action>>();
+            policy.AddState(startState);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. We dont know all the states of Env, and can't keep all states
+            // indexer memory
+            // 2. Hence, policy should very general and with gamma=1
+            // 3. Task is to find an optimal way from start point 
+            // to final state in the windy world.
+
+            // Create an environment
+            WindyGrid env = new WindyGrid();
+            env.Print();
+            State s = env.StartState;
+            int n = 0;
+            while (true)
+            {
+                // Create a policy for selection/expansion phase
+                // deterministic
+                IPolicy policy = new SoftPolicy();
+                // Create a policy for simulation phase
+                IPolicy randomMove = new RandomPolicy();
+                MCTS search = new MCTS(new ProxyWind(env), s, policy, randomMove);
+                search.UpdatePolicy(1000);
+
+                env.PrintPolicy(policy);
+                break;
+                // choose action
+                Action a = policy.GetAction(s);
+                Console.WriteLine(a);
+                s = env.GetReward(s, a).Item1;
+                n++;
+                if (s.Type == StateType.Terminal) break;
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
